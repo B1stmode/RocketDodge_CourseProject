f6486212235d6967b51046c2d7a59a7dacbe6e2f
B1stmode <121379893+B1stmode@users.noreply.github.com>
2025-12-14 16:22:26 +0100

    Settings Menu done
    Finished settings menu and started making the level selection menu

--- Assets/Scripts/ButtonsFunction.cs
+++ Assets/Scripts/ButtonsFunction.cs
@@ -5,39 +5,10 @@ using UnityEngine.Audio;
 using Unity.VisualScripting;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using TMPro;
 public class ButtonsFunction : MonoBehaviour
 {
-    Resolution[] resolutions;
-
     public AudioMixer audioMixer;
-    public Dropdown resolutionDropdown;
-
-    void Start()
-    {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = 1;
-            }
-        }
-
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -50,13 +21,11 @@ public class ButtonsFunction : MonoBehaviour
 
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("Volume", volume);
+        audioMixer.SetFloat("VolumeSlider", volume);
     }
 
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
-
-
 }
