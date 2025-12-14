52366f4227f01ff597e6f431ad34ff960ce54f15
B1stmode <121379893+B1stmode@users.noreply.github.com>
2025-12-14 02:38:06 +0100

    Settings Added
    Added functional settings
    NOTE:
    the resolution function is still not working needs a fix

--- Assets/Scripts/ButtonsFunction.cs
+++ Assets/Scripts/ButtonsFunction.cs
@@ -1,9 +1,62 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using Unity.VisualScripting;
+using System.Collections.Generic;
+using UnityEngine.UI;
 public class ButtonsFunction : MonoBehaviour
 {
+    Resolution[] resolutions;
+
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = 1;
+            }
+        }
+
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
 }
